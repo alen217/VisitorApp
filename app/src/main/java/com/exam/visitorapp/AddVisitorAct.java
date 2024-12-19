c90d0e3f73736d907052c3606c7994483166d906
alen217 <alen217fisat@gmail.com>
2024-12-19 10:34:14 +0530

    ApiCommit

--- app/src/main/java/com/exam/visitorapp/AddVisitorAct.java
+++ app/src/main/java/com/exam/visitorapp/AddVisitorAct.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AddVisitorAct extends AppCompatActivity {
 
@@ -46,8 +55,47 @@ public class AddVisitorAct extends AppCompatActivity {
                 s2=e2.getText().toString();
                 s3=e3.getText().toString();
                 s4=e4.getText().toString();
+
+                if(s1.isEmpty()||s2.isEmpty()||s3.isEmpty()||s4.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All fields are mandatory",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    callApi();
+                }
+            }
+
+            private void callApi() {
+                String Apiurl="https://log-app-demo-api.onrender.com/addvisitor";
+                JSONObject data=new JSONObject();
+                try {
+                    data.put("firstname",s1);
+                    data.put("lastname",s2);
+                    data.put("purpose",s3);
+                    data.put("whomToMeet",s4);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                JsonObjectRequest request=new JsonObjectRequest(
+                        Request.Method.POST,
+                        Apiurl,
+                        data,
+                        response -> Toast.makeText(getApplicationContext(),"Succesfully Added",Toast.LENGTH_LONG).show(),
+                        error -> Toast.makeText(getApplicationContext(),"Something Wrong",Toast.LENGTH_LONG).show()
+                );
+
+                RequestQueue queue= Volley.newRequestQueue(getApplicationContext());
+                queue.add(request);
+
+
+
             }
         });
+ 
 
+    
+    
     }
+
 }
